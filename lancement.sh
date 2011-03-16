8e909afd1ba8d04c98cdcd6a8121a695684f8e3c
boyquotes <webmaster@linux-live-cd.org>
2011-03-16 06:06:10 +0100

    changement nom fichier lancement2.sh en lancement.sh

--- lancement.sh
+++ lancement.sh
@@ -1,50 +1,73 @@
 #!/bin/bash
+rm -rf ./html
+rm -rf ./tmp
+rm -rf ./sql
+
+mkdir -p tmp/spip
+mkdir html
+mkdir sql
+
+> tmp/repohtml.txt
+> tmp/repohtml.sql
+> tmp/repisql.txt
+
+echo 1 > ./j
+echo "0" > ./rub
+echo "2" > ./num
+
+save="0"
+nettoyer="1"
 prendre=$1
 #echo $prendre
-
-for i in `find /iso/sites/codes-libres/paquets/_plugins_/ -mtime -1 | grep -v .svn`; do echo $i | cut -d"/" -f7 | sort -d  ; done > ./changement0.txt
-uniq ./changement0.txt ./changement.txt
-
-while read line
-do
-cp -ra /iso/sites/codes-libres/paquets/_plugins_/$line* /iso/sites/codes-libres/dezip-clean/
-done < ./changement.txt 
-
-
-find /iso/sites/codes-libres/dezip-clean/ -iname ".svn" -exec rm -rf {} \;
-
-if [ $# -eq "1" ]   #  Le nombre d'arguments passés au script
-                        #+ est-il correct ?
+if [ ${save:-0} = 1 ]
 then
-ls /iso/sites/codes-libres/paquets/_plugins_/[$prendre]* | grep ":" | awk '{print $1}' | sed  's/\/iso\/sites\/codes-libres\/paquets\/_plugins_\///g' | sed  's/://g' > repi.txt
+source init-save.sh
 fi
 
+rep="/var/www/clients/client1/web2/vs-dev"
+rep_script="/var/www/clients/client1/web2/vs-script"
+rep_antislash="\/var\/www\/clients\/client1\/web2\/vs-dev\/"
+
+mkdir ./tmp/spip -p
+
+ls -d $rep/[$prendre]*  | sed  "s/$rep_antislash//g" | sed  's/://g' > tmp/repi.txt
+cat tmp/repi.txt
+
 #Remise a zero des fichiers
-> repi0.txt
-> repa.txt
+> tmp/repi0.txt
+> tmp/repa.txt
 > tmp/fait
 
+cat tmp/repi.txt | sed 's/sql//g;s/tmp//g;s/html//g' | sed '/./!d' > tmp/repa.txt
 while read line
 do
-xml_ou_pas=`ls $line/plugin.xml`
+xml_ou_pas=`ls $rep/$line/plugin.xml`
 
 if [ ${xml_ou_pas:-mot} == 'mot' ]
 then
-echo '#'$line >> repi0.txt
-#echo rep vide
+echo '#'$line >> tmp/repi0.txt
 
 else
-echo $line >> repi0.txt
+echo $line >> tmp/repi0.txt
 #echo rep bon
 fi
 
-done < repi.txt
+done < tmp/repi.txt
 
-cat repi0.txt  | grep -v "#" > repa.txt
-cat repi0.txt  | grep -v "#" >> repisql.txt
+cat tmp/repi0.txt  | grep -v "#" > tmp/repa.txt
+cat tmp/repi0.txt  | grep -v "#" >> tmp/repisql.txt
 
-#echo "0" > ./rub
-#echo "2" > ./num
+./php2html.sh $rep && ./html2php.sh && ./php2sql.sh $rep_script
 
-./php2html.sh 
-#&& ./php2sql.sh
+if [ ${nettoyer:-0} = "1" ]
+then
+rm -rf ./html
+rm -rf ./tmp
+rm -rf ./sql
+
+rm -rf ./j
+rm -rf ./rub
+rm -rf ./num
+
+rm -rf ./repophp.txt
+fi
